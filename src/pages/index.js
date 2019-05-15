7ffa712c549a0e0232cba76e46a6ea32e502fa3e
Jesper Johansson <jesper.johansson@strateg.se>
2019-05-15 21:46:57 +0200

    add empty span for testing branch deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,10 @@ import Layout from '../components/Layout'
 export default function IndexPage() {
   return (
     <Layout>
-      <p>Work in progress</p>
+      <p>
+        Work in progress
+        <span style={{ display: 'none' }} />
+      </p>
     </Layout>
   )
 }
