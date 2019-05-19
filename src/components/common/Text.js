c79310a535912eb17e9478d2bb1d7255069b3136
Jesper Johansson <jesper.johansson@strateg.se>
2019-05-19 18:24:05 +0200

    add Text component

---
+++ src/components/common/Text.js
@@ -0,0 +1,52 @@
+/* eslint import/prefer-default-export: 0 */
+import styled from 'styled-components'
+
+export const P = styled.p`
+  margin-bottom: 0.5em;
+  font-size: 1.6rem;
+  line-height: 1.5em;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`
+export const Span = styled.span`
+  font-size: 1.6rem;
+  line-height: 1.5em;
+`
+
+export const H1 = styled.h1`
+  margin: 0.67em 0;
+  font-size: 3.2rem;
+  line-height: 1.25em;
+`
+
+export const H2 = styled.h1`
+  margin: 0.83em 0;
+  font-size: 2.4rem;
+  line-height: 1.25em;
+`
+
+export const H3 = styled.h3`
+  margin: 1em 0;
+  font-size: 1.8rem;
+  line-height: 1.25em;
+`
+
+export const H4 = styled.h4`
+  margin: 1.33em 0;
+  font-size: 1.6rem;
+  line-height: 1.25em;
+`
+
+export const H5 = styled.h5`
+  margin: 1.67em 0;
+  font-size: 1.4rem;
+  line-height: 1.25em;
+`
+
+export const H6 = styled.h6`
+  margin: 2.33em 0;
+  font-size: 1rem;
+  line-height: 1.25em;
+`
